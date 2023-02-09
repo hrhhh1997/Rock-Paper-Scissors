46de8479579049d2aa5cbb191c62cce8d5010666
Nana Kojo <adarkwananakojonyan@yahoo.com>
2023-02-09 14:51:26 +0000

    Create the playRound function

--- javascript/JS.js
+++ javascript/JS.js
@@ -9,3 +9,18 @@ let getComputerChoice = () => {
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    let pToLower = playerSelection.toLowerCase();
+    let cToLower = computerSelection.toLowerCase();
+
+    if (pToLower === cToLower){
+        return "Draw - This game is a Tie!";
+    } else if (pToLower === "rock" && cToLower === "scissors" || pToLower === "paper" && cToLower === "rock" || pToLower === "scissors" && cToLower === "paper"){
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else{
+        return `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+// const cVar = getComputerChoice();
+// console.log(playRound("scissors", cVar));
