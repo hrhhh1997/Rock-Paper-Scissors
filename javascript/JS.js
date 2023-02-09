61249914c02da2e7f93d268f712e861c05cfc228
Nana Kojo <adarkwananakojonyan@yahoo.com>
2023-02-09 14:21:46 +0000

    Create getComputerChoice function

--- javascript/JS.js
+++ javascript/JS.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+let getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 1){
+        return "Rock";
+    } else if (choice === 2){
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
